ae825794fe9acb8de3b2e0fce1a5a25037b3f20b
EmiAK18 <verena.manz@t-online.de>
2026-05-30 23:31:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 let latestAnswer = "Warte auf Antwort...";
 
