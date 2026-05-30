80d250c889f1c58a30000d03db7593a05ac501db
EmiAK18 <verena.manz@t-online.de>
2026-05-30 23:03:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.post('/answer', (req, res) => {
   latestAnswer = req.body.text;
   res.json({ success: true });
 });
-app.get('/answer', (req, res) => {
-  res.json({ text: latestAnswer });
+app.get('/', (req, res) => {
+  res.send(`<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body><p>${latestAnswer}</p></body></html>`);
 });
 app.listen(3000, () => console.log('Server läuft!'));
