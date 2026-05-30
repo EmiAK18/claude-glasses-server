04b254ce1aacb4cbd6c3ba597e9d41ea210eba7a
EmiAK18 <verena.manz@t-online.de>
2026-05-30 22:34:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
 let latestAnswer = "Warte auf Antwort...";
-
 app.post('/answer', (req, res) => {
   latestAnswer = req.body.text;
   res.json({ success: true });
 });
-
 app.get('/answer', (req, res) => {
   res.json({ text: latestAnswer });
 });
-
+app.get('/answer-text', (req, res) => {
+  res.send(latestAnswer);
+});
 app.listen(3000, () => console.log('Server läuft!'));
